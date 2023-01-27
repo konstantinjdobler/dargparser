93dfeaecf04dec422240e688b7b4338732f568b6
Konstantin Dobler <konstantin.j.dobler@gmail.com>
2023-01-27 23:37:35 +0100

    Add specific version number option to release script

--- .github/scripts/release.py
+++ .github/scripts/release.py
@@ -39,22 +39,25 @@ def bump_patch_number(version_number: str) -> str:
 
 
 def create_new_release(args):
-    """Create a new patch release on GitHub."""
-    try:
-        last_version_number = get_last_version()
-    except subprocess.CalledProcessError as err:
-        if err.stderr.decode("utf8").startswith("HTTP 404:"):
-            # The project doesn't have any releases yet.
-            new_version_number = "v0.1.0"
-        else:
-            raise
+    """Create a new release on GitHub."""
+    if args.version:
+        new_version_number = args.version
     else:
-        if args.major:
-            new_version_number = bump_major_number(last_version_number)
-        if args.minor:
-            new_version_number = bump_minor_number(last_version_number)
-        if args.patch:
-            new_version_number = bump_patch_number(last_version_number)
+        try:
+            last_version_number = get_last_version()
+        except subprocess.CalledProcessError as err:
+            if err.stderr.decode("utf8").startswith("HTTP 404:"):
+                # The project doesn't have any releases yet.
+                new_version_number = "v0.1.0"
+            else:
+                raise
+        else:
+            if args.major:
+                new_version_number = bump_major_number(last_version_number)
+            if args.minor:
+                new_version_number = bump_minor_number(last_version_number)
+            if args.patch:
+                new_version_number = bump_patch_number(last_version_number)
 
     subprocess.run(
         ["gh", "release", "create", "--generate-notes", new_version_number],
@@ -68,6 +71,7 @@ if __name__ == "__main__":
     parser.add_argument("--major", action="store_true")
     parser.add_argument("--minor", action="store_true")
     parser.add_argument("--patch", action="store_true")
+    parser.add_argument("--version", type=str, default=None)
 
     args = parser.parse_args()
     create_new_release(args)
